e6757f246d72cbd8a07db7483de597cccd106794
jill64 <intents.turrets0h@icloud.com>
2023-12-18 22:59:51 +0900

    move files

--- src/spc/postprocess/copyViteCoverage.ts
+++ src/spc/postprocess/copyViteCoverage.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 import { VITE_RAW_DIR } from '../../constants.js'
 import { getOutDir } from '../../utils/getOutDir.js'
 import { getTmpDir } from '../../utils/getTmpDir.js'
-import { mv } from './utils/mv.js'
+import { mv } from '../../utils/mv.js'
 
 export const copyViteCoverage = async () => {
   const dist = path.join(getOutDir(), VITE_RAW_DIR)
