14b2bb16840cf29c697769a3fb09eb01f1069b74
jill64 <intents.turrets0h@icloud.com>
2023-12-18 21:30:01 +0900

    additional arg in transformDir

--- src/spc/postprocess/analyze/utils/transformDir.ts
+++ src/spc/postprocess/analyze/utils/transformDir.ts
@@ -5,7 +5,7 @@ import { transformFile } from './transformFile.js'
 export const transformDir = async (
   from: string,
   to: string,
-  fn: (source: string) => string | Promise<string>
+  fn: (source: string, path: string) => string | Promise<string>
 ) => {
   const [list] = await Promise.all([
     readdir(from, {
@@ -22,7 +22,7 @@ export const transformDir = async (
     .map(async (dirent) => {
       const fromPath = path.join(from, dirent.name)
       const toPath = path.join(to, dirent.name)
-      await transformFile(fromPath, toPath, fn)
+      await transformFile(fromPath, toPath, (str) => fn(str, fromPath))
     })
 
   await Promise.all(result)
