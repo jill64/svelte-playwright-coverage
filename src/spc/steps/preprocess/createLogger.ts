ad80add0cc8b73a633668d6fb23a70563b102fab
jill64 <intents.turrets0h@icloud.com>
2023-12-18 14:00:57 +0900

    improve logger

--- src/spc/steps/preprocess/createLogger.ts
+++ src/spc/steps/preprocess/createLogger.ts
@@ -1,11 +1,34 @@
+import kleur from 'kleur'
 import { LogLevel } from '../../../utils/logLevel.js'
 
-const empty = () => {}
+const set = (term: unknown, fn: (str: string) => unknown) =>
+  term ? fn : () => {}
 
-export const createLogger = (logLevel?: LogLevel) => ({
-  ...console,
-  info: logLevel === 'info' || logLevel === 'debug' ? console.info : empty,
-  log: logLevel === 'info' || logLevel === 'debug' ? console.log : empty,
-  warn: logLevel !== 'error' ? console.warn : empty,
-  debug: logLevel === 'debug' ? console.debug : empty
-})
+export const createLogger = (logLevel?: LogLevel) => {
+  const allowInfo = logLevel === 'info' || logLevel === 'debug'
+  const allowWarn = logLevel !== 'error'
+  const allowDebug = logLevel === 'debug'
+
+  return {
+    ...console,
+
+    info: set(allowInfo, console.info),
+
+    /** **S**tyled **S**tring info log */
+    infoS: set(allowInfo, (str: string) => console.info(kleur.blue(str))),
+
+    log: set(allowInfo, console.log),
+
+    warn: set(allowWarn, console.warn),
+
+    /** **S**tyled **S**tring warn log */
+    warnS: set(allowWarn, (str: string) =>
+      console.warn(kleur.bold().yellow(str))
+    ),
+
+    debug: set(allowDebug, console.debug),
+
+    /** **S**tyled **S**tring debug log */
+    debugS: set(allowDebug, (str: string) => console.debug(kleur.gray(str)))
+  }
+}
