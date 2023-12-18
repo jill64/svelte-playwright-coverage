e43e628a8178f6c3378f576ef2c347ef49c294d2
jill64 <intents.turrets0h@icloud.com>
2023-12-18 18:11:06 +0900

    add constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,7 @@ import packageJson from '../package.json' assert { type: 'json' }
 export const APP_NAME = packageJson.name
 
 export const PLAYWRIGHT_RAW_DIR = path.join('playwright', 'raw')
+export const PLAYWRIGHT_RESOLVED_DIR = path.join('playwright', 'resolved')
+
 export const VITE_RAW_DIR = path.join('vite', 'raw')
+export const VITE_RESOLVED_DIR = path.join('vite', 'resolved')
