39b6fefdb3cc01f8ed136903e3add8424e5fe378
jill64 <intents.turrets0h@icloud.com>
2023-12-18 20:42:43 +0900

    redef analyze entry

--- src/spc/steps/postprocess/analyze/index.ts
+++ src/spc/steps/postprocess/analyze/index.ts
@@ -1,12 +1,12 @@
 import { Context } from '../../../types/Context.js'
 import { convert } from './convert/index.js'
 import { merge } from './merge/index.js'
+import { report } from './report/index.js'
 import { resolve } from './resolve/index.js'
-import { write } from './write/index.js'
 
 export const analyze = async (context: Context) => {
-  const resolved = await resolve(context)
-  const merged = await merge(context, resolved)
-  const converted = await convert(context, merged)
-  await write(context, converted)
+  await resolve(context)
+  await merge(context)
+  await convert(context)
+  await report(context)
 }
