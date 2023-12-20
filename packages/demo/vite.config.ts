5714489d1d9a931a94df0ccd6f02443ed58cf184
jill64 <intents.turrets0h@icloud.com>
2023-12-21 02:02:11 +0900

    make monorepo

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
-import { coverage } from '../src/vite/index.js'
+import { coverage } from '../svelte-playwright-coverage/src/vite/coverage'
 
 export default defineConfig({
   plugins: [Inspect(), sveltekit(), coverage()]
