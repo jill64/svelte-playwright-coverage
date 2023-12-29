f8054af85686188eee973ad7032af1260e6c0a67
jill64 <intents.turrets0h@icloud.com>
2023-12-29 21:22:15 +0900

    fix: test command

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { extendsConfig } from '@jill64/playwright-config'
 
 export default extendsConfig({
   webServer: {
-    command: 'spc cover pnpm preview',
+    command: 'npm run spc cover pnpm preview',
     port: 4173
   }
 })
