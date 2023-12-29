5d0aa930492678e37a09a55a02083261a2d0ef54
jill64 <intents.turrets0h@icloud.com>
2023-12-29 21:22:35 +0900

    improve spc cmd

--- packages/svelte-playwright-coverage/src/spc/index.ts
+++ packages/svelte-playwright-coverage/src/spc/index.ts
@@ -1,35 +1,83 @@
+import { App } from '@jill64/ts-cli'
 import { spawn } from 'node:child_process'
 import process from 'node:process'
 import v8 from 'node:v8'
 import { postprocess } from './postprocess/index.js'
 import { preprocess } from './preprocess/index.js'
 import { CloseReason } from './types/CloseReason.js'
-import { SPCOptions } from './types/SPCOptions.js'
 
-export const spc = async (
-  command: string,
-  options?: SPCOptions
-): Promise<number> => {
-  const context = await preprocess(options)
+const trimCommand = (rest?: string[]) => {
+  const command = rest?.join(' ').trim()
 
-  v8.takeCoverage()
+  if (!command) {
+    throw new Error('No command provided')
+  }
 
-  const sub = spawn(command, {
-    stdio: 'inherit',
-    shell: true
-  })
-
-  return new Promise((resolve) => {
-    const close = async (reason: CloseReason) => {
-      v8.stopCoverage()
-
-      const result = await postprocess({ reason, context })
-
-      resolve(result)
-    }
-
-    sub.once('exit', close)
-    process.once('SIGINT', close)
-    process.once('SIGHUP', close)
-  })
+  return command
 }
+
+export const spc = new App(
+  {
+    options: {
+      output: {
+        alias: 'o',
+        type: 'string',
+        description: 'Coverage Output Directory'
+      },
+      quiet: {
+        alias: 'q',
+        description: 'Disable logging'
+      },
+      verbose: {
+        alias: 'V',
+        description: 'Enable verbose logging'
+      }
+    },
+    rest: {
+      placeholder: 'command',
+      description: 'Command to run'
+    }
+  },
+  async ({ options, rest }) => {
+    const command = trimCommand(rest)
+    const context = await preprocess(options)
+
+    const sub = spawn(command, {
+      stdio: 'inherit',
+      shell: true
+    })
+
+    return new Promise((resolve) => {
+      const close = async (reason: CloseReason) => {
+        await postprocess({ reason, context })
+        resolve()
+      }
+
+      sub.once('exit', close)
+      process.once('SIGINT', close)
+      process.once('SIGHUP', close)
+    })
+  }
+).add(
+  'cover',
+  {
+    rest: {
+      placeholder: 'command',
+      description: 'V8 Coverage Measurement Target Command'
+    }
+  },
+  ({ rest }) => {
+    const command = trimCommand(rest)
+
+    v8.takeCoverage()
+
+    const sub = spawn(command, {
+      stdio: 'inherit',
+      shell: true
+    })
+
+    sub.once('exit', () => {
+      v8.stopCoverage()
+    })
+  }
+)
