22e0e854c01626722b32dbc4681b73c1b1a381b9
jill64 <intents.turrets0h@icloud.com>
2024-02-08 13:27:56 +0900

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,12 @@
 import { branchPreview, extendsConfig } from '@jill64/playwright-config'
 
-export default extendsConfig(branchPreview())
+export default extendsConfig(
+  branchPreview({
+    fallback: {
+      webServer: {
+        command: 'npm run spc cover npm run preview',
+        port: 4173
+      }
+    }
+  })
+)
